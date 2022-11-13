4329a82fabecf51e6a664c5b2adeb6a91526c83d
Marko <mo52775@fer.hr>
2022-11-13 17:39:25 +0100

    finish lab2

--- aplikacija/web-app-backend/dbQuery.js
+++ aplikacija/web-app-backend/dbQuery.js
@@ -5,4 +5,74 @@ module.exports = class dbq {
     static async getAll() {
         return await db.query('SELECT * FROM automobili JOIN motori ON (automobili.id_automobila = motori.id_auta)')
     }
+
+    static async getSearchWildcard(searchTerm) {
+        searchTerm = searchTerm.toLowerCase()
+        
+        return await db.query(`SELECT * FROM automobili JOIN motori ON (automobili.id_automobila = motori.id_auta) WHERE 
+        LOWER(id_automobila::text) LIKE '%${searchTerm}%' OR 
+        LOWER(proizvodac) LIKE '%${searchTerm}%' OR 
+        LOWER(naziv_modela) LIKE '%${searchTerm}%' OR 
+        LOWER(pocetak_proizvodnje::text) LIKE '%${searchTerm}%' OR 
+        LOWER(zavrsetak_proizvodnje::text) LIKE '%${searchTerm}%' OR 
+        LOWER(broj_sjedala::text) LIKE '%${searchTerm}%' OR 
+        LOWER(broj_vrata::text) LIKE '%${searchTerm}%' OR 
+        LOWER(min_velicina_prtljaznika::text) LIKE '%${searchTerm}%' OR 
+        LOWER(max_velicina_prtljaznika::text) LIKE '%${searchTerm}%' OR 
+        LOWER(duljina::text) LIKE '%${searchTerm}%' OR 
+        LOWER(sirina::text) LIKE '%${searchTerm}%' OR 
+        LOWER(visina::text) LIKE '%${searchTerm}%' OR 
+        LOWER(id_motora::text) LIKE '%${searchTerm}%' OR 
+        LOWER(gorivo) LIKE '%${searchTerm}%' OR 
+        LOWER(broj_cilindri::text) LIKE '%${searchTerm}%' OR 
+        LOWER(zapremina::text) LIKE '%${searchTerm}%' OR 
+        LOWER(snaga::text) LIKE '%${searchTerm}%' OR 
+        LOWER(okretni_moment::text) LIKE '%${searchTerm}%'`)
+    }
+
+    static async getSearchAttribute(searchTerm, attribute) {
+        searchTerm = searchTerm.toLowerCase()
+
+        return await db.query(`SELECT * FROM automobili JOIN motori ON (automobili.id_automobila = motori.id_auta) WHERE
+        LOWER(${attribute}::text) LIKE '%${searchTerm}%'`)
+    }
+
+    static async getSearchAttributeJSON(searchTerm, attribute) {
+        searchTerm = searchTerm.toLowerCase()
+        
+        return await db.query(`SELECT array_to_json(array_agg(row_to_json(entitet)))
+        FROM (SELECT automobili.*, array_to_json(array_agg(to_json(motori.*))) as motori
+        FROM automobili 
+        INNER JOIN motori on automobili.id_automobila = motori.id_auta
+        WHERE LOWER(${attribute}::text) LIKE '%${searchTerm}%' 
+        GROUP BY automobili.id_automobila) entitet`)
+    }
+
+    static async getSearchWildcardJSON(searchTerm) {
+        searchTerm = searchTerm.toLowerCase()
+        
+        return await db.query(`SELECT array_to_json(array_agg(row_to_json(entitet)))
+        FROM (SELECT automobili.*, array_to_json(array_agg(to_json(motori.*))) as motori
+        FROM automobili 
+        INNER JOIN motori on automobili.id_automobila = motori.id_auta
+        WHERE LOWER(id_automobila::text) LIKE '%${searchTerm}%' OR 
+        LOWER(proizvodac) LIKE '%${searchTerm}%' OR 
+        LOWER(naziv_modela) LIKE '%${searchTerm}%' OR 
+        LOWER(pocetak_proizvodnje::text) LIKE '%${searchTerm}%' OR 
+        LOWER(zavrsetak_proizvodnje::text) LIKE '%${searchTerm}%' OR 
+        LOWER(broj_sjedala::text) LIKE '%${searchTerm}%' OR 
+        LOWER(broj_vrata::text) LIKE '%${searchTerm}%' OR 
+        LOWER(min_velicina_prtljaznika::text) LIKE '%${searchTerm}%' OR 
+        LOWER(max_velicina_prtljaznika::text) LIKE '%${searchTerm}%' OR 
+        LOWER(duljina::text) LIKE '%${searchTerm}%' OR 
+        LOWER(sirina::text) LIKE '%${searchTerm}%' OR 
+        LOWER(visina::text) LIKE '%${searchTerm}%' OR 
+        LOWER(id_motora::text) LIKE '%${searchTerm}%' OR 
+        LOWER(gorivo) LIKE '%${searchTerm}%' OR 
+        LOWER(broj_cilindri::text) LIKE '%${searchTerm}%' OR 
+        LOWER(zapremina::text) LIKE '%${searchTerm}%' OR 
+        LOWER(snaga::text) LIKE '%${searchTerm}%' OR 
+        LOWER(okretni_moment::text) LIKE '%${searchTerm}%' 
+        GROUP BY automobili.id_automobila) entitet`)
+    }
 }
--- aplikacija/web-app-backend/server.js
+++ aplikacija/web-app-backend/server.js
@@ -6,7 +6,8 @@ const port = 3001
 const dbq = require('./dbQuery')
 const cors = require('cors')
 
-app.use(bodyParser.urlencoded({ extended: false }))
+//app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.json())
 app.use(cors())
 
 app.get('/csv', (req, res) => {
@@ -20,6 +21,30 @@ app.get('/json', (req, res) => {
 app.get('/automobili/getall', async (req, res) => {
     let alldata = await dbq.getAll()
     res.send(alldata.rows)
+})
+
+app.post('/automobili/search', async (req, res) => {
+    let result
+
+    if (req.body.atribut == 'all') {
+        result = await dbq.getSearchWildcard(req.body.searchTerm)
+    } else {
+        result = await dbq.getSearchAttribute(req.body.searchTerm, req.body.atribut)
+    }
+
+    res.send(result.rows)
+})
+
+app.post('/automobili/search/json', async (req, res) => {
+    let result
+    
+    if (req.body.atribut == 'all') {
+        result = await dbq.getSearchWildcardJSON(req.body.searchTerm)
+    } else {
+        result = await dbq.getSearchAttributeJSON(req.body.searchTerm, req.body.atribut)
+    }
+
+    res.send(result.rows[0].array_to_json)
 })
 
 app.listen(port, () => {
